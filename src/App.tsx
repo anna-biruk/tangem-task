f436b5e120830611420f4b9fa6f5159ab70677ce
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-02-06 20:30:57 +0100

    added scrolling feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import HeaderBanner from "./components/HeaderBanner";
 import Banner from "./components/Banner";
 
 function App() {
+  const [showFooterBanner, setShowFooterBanner] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const headerBannerHeight = 55;
+
+      if (scrollPosition > headerBannerHeight) {
+        setShowFooterBanner(true);
+      } else {
+        setShowFooterBanner(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <HeaderBanner />
-      <div style={{ marginTop: "650px" }}>
-        {" "}
-        <Banner />
-      </div>
+      <div style={{ marginTop: "550px" }}>{showFooterBanner && <Banner />}</div>
     </div>
   );
 }
